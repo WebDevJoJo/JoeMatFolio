d9a796e42982fa7b18e26e1125afeb6a88696c21
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-04-05 17:35:12 +0200

    access component written (access is the new login)

--- src/app/personal-area/access/access.component.ts
+++ src/app/personal-area/access/access.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
 
+interface User {
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-access',
   templateUrl: './access.component.html',
-  styleUrls: ['./access.component.scss']
+  styleUrls: ['./access.component.scss'],
 })
 export class AccessComponent {
+  newEmail: string = '';
+  password: string = '';
+  signedEmail: string = '';
+  newPassword: string = '';
+  confirmPassword: string = '';
+  phoneNumber: string = '';
+  address: string = '';
+  newPasswordMessagge: string = '';
+  confirmMessage: string = '';
+  passwordMessage: string = '';
+  users: User[] = [
+    { email: 'joelemelchiorre@gmail.com', password: 'passwordjoele' },
+    { email: 'chiocciola@mail.it', password: 'passwordmattia' },
+    { email: 'user@provider.domain', password: 'passworduser3' },
+  ];
 
+  CheckPass() {
+    if (this.newPassword.length < 10) {
+      this.newPasswordMessagge = 'Too short';
+    } else {
+      this.newPasswordMessagge = 'Good';
+    }
+  }
+
+  SamePass() {
+    if (this.newPassword == this.confirmPassword) {
+      this.confirmMessage = 'Passwords are equal';
+    } else {
+      this.confirmMessage = 'Passwords are different';
+    }
+  }
+
+  Access() {
+    const user = this.users.find(
+      (utente) =>
+        utente.email === this.signedEmail && utente.password === this.password
+    );
+    if (user) {
+      console.log('Login success');
+    } else {
+      console.log('Login failed');
+    }
+  }
+
+  CheckLogin() {
+    if (this.password.length < 10) {
+      this.passwordMessage = 'Invalid credentials';
+    }
+  }
 }
