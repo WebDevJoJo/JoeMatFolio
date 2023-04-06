7bde5ab975dd4544fa2d581d6ec1715ca21fe74e
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-04-06 15:39:16 +0200

    access component re/written + more semantic variables

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface User {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-access',
   templateUrl: './access.component.html',
-  styleUrls: ['./access.component.scss']
+  styleUrls: ['./access.component.scss'],
 })
 export class AccessComponent {
+  newEmail: string = '';
+  loginpasswordts: string = '';
+  signedEmail: string = '';
+  newPassword: string = '';
+  confirmPassword: string = '';
+  phoneNumber: string = '';
+  address: string = '';
+  newPasswordMessagge: string = '';
+  confirmMessage: string = '';
+  passwordMessage: string = '';
+  userlist: User[] = [];
+  constructor(private http: HttpClient) {}
+  checkPasswordLenght() {
+    if (this.newPassword.length < 10) {
+      this.newPasswordMessagge = 'Too short';
+    } else {
+      this.newPasswordMessagge = 'Good';
+    }
+  }
 
+  checkPasswordsEquality() {
+    if (this.newPassword == this.confirmPassword) {
+      this.confirmMessage = 'Passwords are equal';
+    } else {
+      this.confirmMessage = 'Passwords are different';
+    }
+  }
+
+  accessResult() {
+    this.http
+      .get<{ userlist: User[] }>('../../assets/users.json')
+      .subscribe((users) => {
+        const matchedUser = users.userlist.find(
+          (user) =>
+            user.email === this.signedEmail &&
+            user.password === this.loginpasswordts
+        );
+        if (matchedUser) {
+          console.log('Login successful');
+        } else {
+          console.log('Login failed');
+        }
+      });
+  }
+
+  checkLoginCredentials() {
+    if (this.loginpasswordts.length < 10) {
+      this.passwordMessage = 'Invalid credentials';
+    }
+  }
 }
--- src/app/access/access.module.ts
+++ src/app/access/access.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AccessComponent } from './access.component';
 
-
-
 @NgModule({
-  declarations: [
-    AccessComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AccessComponent],
+  imports: [CommonModule, FormsModule],
 })
-export class AccessModule { }
+export class AccessModule {}
