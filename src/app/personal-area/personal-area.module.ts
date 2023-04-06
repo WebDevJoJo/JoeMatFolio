758d307c917b2ff48b16ef59d8e3d367f16100ba
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-04-06 15:14:29 +0200

    access module re-created outside of personal area

--- src/app/personal-area/personal-area.module.ts
+++ src/app/personal-area/personal-area.module.ts
@@ -2,25 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { AccessComponent } from './access/access.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PreventiveComponent } from './preventive/preventive.component';
 import { PaymentComponent } from './payment/payment.component';
 
 const routes: Routes = [
-  { path: '', component: AccessComponent },
-  { path: 'welcome', component: WelcomeComponent },
+  { path: '', component: WelcomeComponent },
   { path: 'preventive', component: PreventiveComponent },
   { path: 'payment', component: PaymentComponent },
 ];
 
 @NgModule({
-  declarations: [
-    AccessComponent,
-    WelcomeComponent,
-    PreventiveComponent,
-    PaymentComponent,
-  ],
+  declarations: [WelcomeComponent, PreventiveComponent, PaymentComponent],
   imports: [CommonModule, RouterModule.forChild(routes), FormsModule],
 })
 export class PersonalAreaModule {}
