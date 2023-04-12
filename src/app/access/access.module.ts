d603a820216bf4f5aa7757aaa706bf21ea6f5d2a
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-04-12 15:16:47 +0200

    routermodule import rewritten

--- src/app/access/access.module.ts
+++ src/app/access/access.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AccessComponent } from './access.component';
-import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: '', component: AccessComponent }];
 
