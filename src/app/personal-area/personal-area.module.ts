dd1644f017584c98c691a2205bde128c9fabaa31
webdevjojo <98335026+WebDevJoJo@users.noreply.github.com>
2023-05-12 09:44:12 +0200

    angular material implementation in personal area module

--- src/app/personal-area/personal-area.module.ts
+++ src/app/personal-area/personal-area.module.ts
@@ -2,26 +2,53 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PreventiveComponent } from './preventive/preventive.component';
-import { PaymentComponent } from './payment/payment.component';
-import { CompaniesComponent } from './companies/companies.component';
+import { PaymentMethodsComponent } from './payment-methods/payment-methods.component';
+import {
+  DialogCompany,
+  PartnerCompaniesComponent,
+} from './partner-companies/partner-companies.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
+  { path: 'welcome', component: WelcomeComponent },
   { path: 'preventive', component: PreventiveComponent },
-  { path: 'payment', component: PaymentComponent },
-  { path: 'companies', component: CompaniesComponent },
+  { path: 'payment', component: PaymentMethodsComponent },
+  { path: 'companies', component: PartnerCompaniesComponent },
 ];
 
 @NgModule({
   declarations: [
-    WelcomeComponent,
     PreventiveComponent,
-    PaymentComponent,
-    CompaniesComponent,
+    PaymentMethodsComponent,
+    PartnerCompaniesComponent,
+    WelcomeComponent,
+    DialogCompany,
   ],
-  imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatListModule,
+  ],
 })
 export class PersonalAreaModule {}
